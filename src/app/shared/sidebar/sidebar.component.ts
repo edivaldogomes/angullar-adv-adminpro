45e75c58523e5fde908d7f6f720f2758eef8a047
Edivaldo Gomes <edslavianov@gmail.com>
2023-07-20 17:22:23 +0200

    create sidebar service and submenu

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { SidebarService } from 'src/app/services/sidebar.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent {
-
+  menuItems: any[];
+  constructor(private sidebarService: SidebarService) {
+    this.menuItems = sidebarService.menu;
+    console.log('A MOSTRAR', this.menuItems);
+  }
 }
