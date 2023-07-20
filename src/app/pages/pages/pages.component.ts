4e92694988b76e3c7d97cd948a0f130f32401bf6
Edivaldo Gomes <edslavianov@gmail.com>
2023-07-20 18:04:36 +0200

    Using Scripts from imported files in the index.html in TypeScript

--- src/app/pages/pages/pages.component.ts
+++ src/app/pages/pages/pages.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { SettingsService } from 'src/app/services/settings.service';
 
+declare function customInitFunctions(): any;
+
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
   styleUrls: ['./pages.component.css'],
 })
-export class PagesComponent {
+export class PagesComponent implements OnInit {
   constructor(private settingService: SettingsService) {}
+
+  ngOnInit(): void {
+    customInitFunctions();
+  }
 }
